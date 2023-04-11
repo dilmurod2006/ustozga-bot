50eef02186a51bd4a5e1abcc44718c57c4dbe399
dilmurod2006 <115336811+dilmurod2006@users.noreply.github.com>
2023-04-11 16:57:21 +0500

    Update text.py

--- text.py
+++ text.py
@@ -2,28 +2,25 @@ import telebot
 import pyttsx3
 from pydub import AudioSegment
 
-# Telebot kutubxonasi bilan bog'lanish
-bot = telebot.TeleBot("6261129884:AAFPC_91q0trhW1ByTV-zRandOLHDbltkyg")
 
-# Pyttsx3 kutubxonasi bilan bog'lanish
+token = "6261129884:AAFPC_91q0trhW1ByTV-zRandOLHDbltkyg"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hello!')
 engine = pyttsx3.init()
-
-# Matnni ovozga aylantirish va ogg formatida saqlash funktsiyasi
-def text_to_speech(text):
-    engine.save_to_file(text, 'audio.wav')
+#  text to speech and audio send message.chat.id
+@bot.message_handler(content_types=['text'])
+def text_to_speech(message):
+    engine.save_to_file(message, 'audio.wav')
     engine.runAndWait()
     sound = AudioSegment.from_wav('audio.wav')
     sound.export('audio.ogg', format='ogg')
 
     audio = open('audio.ogg', 'rb')
     return audio
+    bot.send_audio(message.chat.id, audio)
 
-# Matn yuborilganda ishga tushiriladigan funktsiya
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    text = message.text
-    audio = text_to_speech(text)
-    bot.send_voice(message.chat.id, audio)
-
-# Botni ishga tushirish
 bot.polling()
